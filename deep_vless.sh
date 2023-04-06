ad551a0f2edad5b44032deafdfdc9c347324c702
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-06 18:34:34 +0330

    Update deep_vless.sh

--- deep_vless.sh
+++ deep_vless.sh
@@ -20,8 +20,19 @@ yellow() {
 run_v2ray() {
     clear
     echo "#############################################################"
-    echo -e "#              ${RED} Deepnote v2ray 一键安装脚本${PLAIN}                 #"
-    # ... (other echo statements) ...
+echo -e "#              ${RED} Deepnote v2ray 一键安装脚本${PLAIN}                 #"
+echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
+echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
+echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
+echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakablogchannel             #"
+echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoxpz                   #"
+echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
+echo "#############################################################"
+echo ""
+
+yellow "使用前请注意："
+red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
+red "2. 我不保证脚本其搭建节点的稳定性"
 
     rm -f web config.json
     yellow "开始安装..."
