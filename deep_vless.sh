691480648616898dacd92f8c87d849b436430c7e
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-06 18:40:58 +0330

    Update deep_vless.sh

--- deep_vless.sh
+++ deep_vless.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-PLAIN='\033[0m'
-
-red() {
-    echo -e "\033[31m\033[01m$1\033[0m"
-}
-
-green() {
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-
-yellow() {
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-
-clear
-echo "#############################################################"
-echo -e "#              ${RED} Deepnote v2ray 一键安装脚本${PLAIN}                 #"
-echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
-echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
-echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
-echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakablogchannel             #"
-echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoxpz                   #"
-echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
-echo "#############################################################"
-echo ""
-
-yellow "使用前请注意："
-red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
-red "2. 我不保证脚本其搭建节点的稳定性"
-
     rm -f web config.json
     yellow "开始安装..."
     wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
